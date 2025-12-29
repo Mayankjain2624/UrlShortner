d6ca74ebb96254ed21a8e283c4125e20c96435b3
Mayank Jain <mayankjain4020@gmail.com>
2025-12-30 01:56:16 +0530

    schema added

--- models/url.js
+++ models/url.js
@@ -1,7 +1,17 @@
-const mongoose=require('mongoose');
-const urlSchema=new mongoose.Schema(
-    {
-
-    }
+const mongoose = require("mongoose");
+const urlSchema = new mongoose.Schema(
+  {
+    shortId: {
+      type: string,
+      required: true,
+      unique: true,
+    },
+    redirectUrl: {
+      type: string,
+      required: true,
+    },
+    visitHistory: [{ timestamp: { type: Number } }],
+  },
+  { timestamps: true }
 );
-module.exports=mongoose.model('Url', urlSchema); 
+module.exports = mongoose.model("Url", urlSchema);
